4f3b55c321e08f75f7de5fdea37a5b102cbc2a28
Urbantyrone <127332605+Urbantyrone@users.noreply.github.com>
2023-03-19 17:15:51 +0100

    Add integration to product-list.component.ts

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../../services/product.service";
+import {Product} from "../../common/product";
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
 
+// Integrate service with Angular component
+export class ProductListComponent implements OnInit{
+
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {
+  }
+
+  ngOnInit(): void{
+    this.listProducts();
+  }
+
+  // assign my results to the Product array
+  listProducts() {
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
 }
